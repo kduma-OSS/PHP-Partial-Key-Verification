1555159adf6ac02d5e63107d42685dada4924946
Krystian Duma <kduma@users.noreply.github.com>
2025-09-07 05:36:35 +0200

    Assert seed is valid

--- tests/Generator/CrossPlatformCodeGeneratorTest.php
+++ tests/Generator/CrossPlatformCodeGeneratorTest.php
@@ -6,13 +6,15 @@ namespace Tests\PKV\Generator;
 
 use KDuma\PKV\Checksum\Adler16;
 use KDuma\PKV\Generator\PartialKeyGenerator;
+use KDuma\PKV\Hash\Fnv1a;
 use KDuma\PKV\Hash\Jenkins96;
+use KDuma\PKV\PartialKeyValidator;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
 #[CoversClass(\KDuma\PKV\Generator\CodeGenerator::class)] final class CrossPlatformCodeGeneratorTest extends TestCase
 {
-    public function test_empty_when_no_verified_keys(): void
+    public function test_cross_platform_code(): void
     {
         $generator = PartialKeyGenerator::fromSingleHash(
             new Adler16,
@@ -24,6 +26,11 @@ use PHPUnit\Framework\TestCase;
 
         $key = $generator->generateFromString('bob@smith.com');
 
+        $seed = new Fnv1a()->compute('bob@smith.com');
+        $seed_from_key = PartialKeyValidator::getSerialNumberFromKey($key);
+
         $this->assertSame('QDKZUO-JLLWPY-XWOULC-ONCQIN-5R5X35-ZS3KEQ', $key);
+        $this->assertSame(966448512, $seed);
+        $this->assertSame(966448512, $seed_from_key);
     }
 }
